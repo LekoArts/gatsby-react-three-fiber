08959e738e1b87634c59fe4d9404d6390a782c29
LekoArts <lekoarts@gmail.com>
2019-07-12 20:06:17 +0200

    material improvements

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,15 +1,17 @@
-import React, { useRef } from 'react'
-import { useThree, useRender, extend } from 'react-three-fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import React, { useRef } from "react"
+import { useThree, useRender, extend } from "react-three-fiber"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 extend({ OrbitControls })
 
 const Controls = props => {
-  const { camera, scene } = useThree()
+  const { camera } = useThree()
   const ref = useRef()
-  
-  useRender(() => ref.current && ref.current.update())
-  
+
+  useRender(() => {
+    ref.current && ref.current.update()
+  })
+
   return <orbitControls ref={ref} args={[camera]} {...props} />
 }
 
